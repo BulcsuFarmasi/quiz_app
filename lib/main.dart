5b9933c75bec5470210bc0ebd7d5d4296746a497
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-12-26 22:46:14 +0100

    use final and const for variables

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
+    const questions = [
       {
         'questionText': 'What\'s your favorite color?',
         'answers': ['Black', 'Red', 'Green', 'White']
@@ -42,6 +42,10 @@ class _MyAppState extends State<MyApp> {
         'answers': ['Max', 'Max', 'Max', 'Max']
       },
     ];
+    // var dummy = const ['Hello'];
+    // dummy.add('Max');
+    // print(dummy);
+    // questions = []; // does not work if questions is a const
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('My First App')),
